fc923a99a04d215ac5a7df2c8c7fb308c4b0b3be
Rodolfo Faria <rodolfogf@gmail.com>
2022-09-13 13:51:00 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import NotificationButton from "./compents/NotificationButton"
+import Header from "./compents/Header"
+import SalesCard from "./compents/SalesCard"
 
 function App() {
   return(
     <>
-    <h1>Teste</h1>
-    <NotificationButton/>
+    <Header/>
+    <main>
+        <section id="sales">
+            <div className="dsmeta-container">
+              <SalesCard/>
+            </div>
+        </section>
+    </main>
     </>
   )
-}
+} 
 
 export default App
